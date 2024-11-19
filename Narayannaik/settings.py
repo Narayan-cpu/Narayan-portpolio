478b400e622775a1e1e78e96a6b108c36a2b18df
narayan naik <125474254+Narayan-cpu@users.noreply.github.com>
2024-11-19 22:13:48 +0530

    Update settings.py

--- Narayannaik/settings.py
+++ Narayannaik/settings.py
@@ -70,10 +70,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('DB_USER', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres.xjwcweimbeahmsnzecpk'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'Narayan123@04!'),
         'HOST': os.getenv('DB_HOST', 'aws-0-ap-south-1.pooler.supabase.com'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'PORT': os.getenv('DB_PORT', '6543'),
     }
 }
 
